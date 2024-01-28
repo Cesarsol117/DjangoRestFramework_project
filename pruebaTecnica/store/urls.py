8efc1b6599d6f1b951eafa26c93e8e52bcc3b7b4
Cesarsol117 <cesar_p117@hotmail.com>
2024-01-28 11:24:03 -0500

    CRUD de clients, products, bills y billsProducts

--- pruebaTecnica/store/urls.py
+++ pruebaTecnica/store/urls.py
@@ -5,9 +5,19 @@ from .views import *
 # el nombre que va en el navegador, la vista que acompaña, y un endpoint para mostar o llamar.
 
 urlpatterns = [
-   path('new-client/', ClientCreateAPIView.as_view(), name='new_clients'),
-   path('clients/', ClientAPIView.as_view(), name='all_clients'),
-   path('detail-client/<int:id>', ClientGetAPIView.as_view(), name='one_clients'),
-   path('clients/<int:id>/', ClientPutAPIView.as_view(), name='client-put'),
-   path('delete-clients/<int:id>/', ClientDeleteAPIView.as_view(), name='client-Delete'),
+    path('new-client/', ClientCreateAPIView.as_view(), name='new_clients'),
+    path('clients/', ClientAPIView.as_view(), name='all_clients'),
+    path('detail-client/<int:id>', ClientGetAPIView.as_view(), name='one_clients'),
+    path('clients/<int:id>/', ClientPutAPIView.as_view(), name='client-put'),
+    path('delete-clients/<int:id>/', ClientDeleteAPIView.as_view(), name='client-Delete'),
+#    urls de Bilss
+    path('all-bills/', BillReadAPIView.as_view(), name='all_bills'),
+    path('new-bills/', BillCreateAPIView.as_view(), name='new_bills'),
+    path('update-bills/<int:bill_id>/', BillUpdateAPIView.as_view(), name='update_bills'),
+    path('delete-bills/<int:bill_id>/', BillDeleteAPIView.as_view(), name='delete_bills'),
+    # urls de products
+    path('products/', ProductListCreateAPIView.as_view(), name='product_list_create'),
+    path('products/<int:product_id>/', ProducReadUpdateDeleteAPIView.as_view(), name='product_retrieve_update_destroy'),
+    # tabla pivote
+    path('billproducts/', BillProductListCreateAPIView.as_view(), name='billproduct-list-create'),
 ]
--- pruebaTecnica/store/views.py
+++ pruebaTecnica/store/views.py
@@ -89,3 +89,150 @@ class ClientDeleteAPIView(APIView):
             return Response({'message': 'Client deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
         except Client.DoesNotExist:
             return Response({'error': 'Client not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+# Bills o facturas
+class BillReadAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        bills = Bill.objects.all()
+
+        # Serializa todas las facturas y devuelve la respuesta
+        serializer = BillSerializer(bills, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+# create
+class BillCreateAPIView(APIView):
+    def get(self, request):
+            return Response(data = {
+                                    "client": 1,
+                                    "company_name": "Default",
+                                    "nit": 7894,
+                                    "code": 963
+                                  })
+    
+    def post(self, request):
+        
+
+        # Supongamos que el cliente_id se pasa como parte de los datos de la solicitud.
+        client_id = request.data.get('client', None)
+
+        if client_id is not None:
+            # Intenta obtener el cliente con el client_id.
+            try:
+                client = Client.objects.get(pk=client_id)
+            except Client.DoesNotExist:
+                return Response({'error': 'Client not found'}, status=status.HTTP_404_NOT_FOUND)
+            
+            # Asigna el cliente a la factura antes de guardarla.
+            request.data['client'] = client.id
+
+            serializer = BillSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error': 'client_id is required'}, status=status.HTTP_400_BAD_REQUEST)
+# Update
+class BillUpdateAPIView(APIView):
+    def get(self, request, bill_id):    
+        try:
+            one_bill = Bill.objects.get(id = bill_id)
+            bill_serializer = BillSerializer(one_bill)
+            return Response(bill_serializer.data, status=status.HTTP_200_OK)
+        except Client.DoesNotExist:
+            return Response({'error': 'Client not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    def put(self, request, bill_id):
+        try:
+            # Intenta obtener la factura con el bill_id
+            bill = Bill.objects.get(id=bill_id)
+
+            # Actualiza la factura con los nuevos datos
+            serializer = BillSerializer(bill, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except Bill.DoesNotExist:
+            return Response({'error': 'Bill not found'}, status=status.HTTP_404_NOT_FOUND)
+# delete
+
+class BillDeleteAPIView(APIView):
+    def get(self, request, bill_id):    
+        try:
+            one_bill = Bill.objects.get(id = bill_id)
+            bill_serializer = BillSerializer(one_bill)
+            return Response(bill_serializer.data, status=status.HTTP_200_OK)
+        except Client.DoesNotExist:
+            return Response({'error': 'Client not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+    def delete(self, request, bill_id):
+        try:
+            # Intenta obtener la factura con el bill_id
+            bill = Bill.objects.get(id=bill_id)
+
+            # Elimina la factura
+            bill.delete()
+            return Response({'message': 'Bill deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+        except Bill.DoesNotExist:
+            return Response({'error': 'Bill not found'}, status=status.HTTP_404_NOT_FOUND)
+# CRUD Productos
+
+class ProductListCreateAPIView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProducReadUpdateDeleteAPIView(APIView):
+    def get_object(self, product_id):
+        try:
+            return Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            return None
+
+    def get(self, request, product_id):
+        product = self.get_object(product_id)
+        if product:
+            serializer = ProductSerializer(product)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, product_id):
+        product = self.get_object(product_id)
+        if product:
+            serializer = ProductSerializer(product, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, product_id):
+        product = self.get_object(product_id)
+        if product:
+            product.delete()
+            return Response({'message': 'Product deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+        return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+# tabla pivote
+class BillProductListCreateAPIView(APIView):
+    def get(self, request):
+        bill_products = BillProduct.objects.all()
+        serializer = BillProductSerializer(bill_products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = BillProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
